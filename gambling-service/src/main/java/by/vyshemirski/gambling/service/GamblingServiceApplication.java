4b34cdf33876307b123348f70a4a35f239b033cb
Aliaksei Vyshemirski <vyshemirski.lesha@gmail.com>
2022-06-07 17:39:29 +0300

    feat: create microservice modules

---
+++ gambling-service/src/main/java/by/vyshemirski/gambling/service/GamblingServiceApplication.java
@@ -0,0 +1,13 @@
+package by.vyshemirski.gambling.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamblingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamblingServiceApplication.class, args);
+    }
+
+}
